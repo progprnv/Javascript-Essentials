c06bff1c3e8163ecd49f287ece4e91950d824c65
progprnv <pranavpranav29907@gmail.com>
2024-07-12 23:33:11 +0530

    js

---
+++ commenting.js
@@ -0,0 +1,21 @@
+//this is in-line comment
+//hello world
+
+/* hello
+world
+*/ 
+
+/* this is for multi-line
+comment
+*/ 
+
+
+//This fn below is for print.
+
+function print()
+{
+    
+    console.log("this is print";)
+
+
+}
